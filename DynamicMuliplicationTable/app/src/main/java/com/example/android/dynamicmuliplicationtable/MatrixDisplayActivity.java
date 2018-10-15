a6f154e5a44755f734f3b6960d507e92e0b76b91
rohegde7 <hegde.rohit7@gmail.com>
2018-10-15 18:01:32 +0530

    Scroll Views added for fixed row and column. Problem: scroll not working

--- DynamicMuliplicationTable/app/src/main/java/com/example/android/dynamicmuliplicationtable/MatrixDisplayActivity.java
+++ DynamicMuliplicationTable/app/src/main/java/com/example/android/dynamicmuliplicationtable/MatrixDisplayActivity.java
@@ -2,9 +2,15 @@ package com.example.android.dynamicmuliplicationtable;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.android.dynamicmuliplicationtable.Util.IntegerUtil;
 import com.example.android.dynamicmuliplicationtable.databinding.ActivityMainBinding;
@@ -12,9 +18,13 @@ import com.example.android.dynamicmuliplicationtable.databinding.ActivityMatrixD
 import com.example.android.dynamicmuliplicationtable.models.DimentionsOfMatrix;
 import com.example.android.dynamicmuliplicationtable.models.Table;
 
+import org.w3c.dom.Text;
+
 public class MatrixDisplayActivity extends AppCompatActivity {
 
     ActivityMatrixDisplayBinding mBinding;
+    private int mRow=100, mColumn=100;
+    int mTableMatrix[][];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +38,163 @@ public class MatrixDisplayActivity extends AppCompatActivity {
         //int r = Integer.parseInt(dOfMatrix.rows.get().toString());
         //int c = Integer.parseInt(dOfMatrix.columns.get().toString());
 
-        Table matrixTable = new Table(row,row, getBaseContext(), mBinding);
+        Table matrixTable = new Table(row,row);
         mBinding.setMatrixTable(matrixTable);
+
+        this.mRow = matrixTable.mRow;
+        this.mColumn = matrixTable.mColumn;
+
+        //mTableMatrix = mBinding.getMatrix();
+
+        createTableLayout();
+    }
+
+    private void createBasiclayout(){
+        TableLayout tableLayoutMain = (TableLayout) mBinding.rootViewTableLayout;
+
+        //creating the A section table:
+        TableLayout tableA = new TableLayout(this);
+        tableA.setId(R.id.tableA);
+
+        TableRow tableRowA = new TableRow(this);
+        tableRowA.setId(R.id.tableRowA);
+
+        TextView textViewA = new TextView(this);
+        textViewA.setId(R.id.textViewA);
+        textViewA.setText("MultiTable");
+        textViewA.setPadding(20,20,20,20);
+
+        tableRowA.addView(textViewA);
+        tableA.addView(tableRowA);
+        //A Section table created
+
+        //adding the A section table to the MAIN table:
+        TableRow tableRowMain1 = new TableRow(this);
+        tableRowMain1.setId(R.id.tableRowMain1);
+
+        tableRowMain1.addView(tableA);
+
+        //added A section to the main table layout
+
+        //creating the COMPONENT B,C,D and adding it to the MainROW view
+        HorizontalScrollView horizontalScrollViewB = new HorizontalScrollView(this);
+        horizontalScrollViewB.setBackgroundColor(Color.RED);
+        horizontalScrollViewB.setId(R.id.horizontalScrollViewB);
+        tableRowMain1.addView(horizontalScrollViewB);
+        //created the B view - horizontalScrollViewB and added to the 1st COLUMN
+
+        //creating section C - scroll view and adding it to the main table layout:
+        TableRow tableRowMain2 = new TableRow(this);
+        tableRowMain2.setId(R.id.tableRowMain2);
+
+        ScrollView scrollViewC = new ScrollView(this);
+        scrollViewC.setId(R.id.scrollViewC);
+
+        tableRowMain2.addView(scrollViewC);
+        //done creating and adding it to the MAIN table layout
+
+        //creating the section D and adding it to the RowC layout:
+        ScrollView scrollViewD = new ScrollView(this);
+        scrollViewD.setId(R.id.scrollViewD);
+        HorizontalScrollView horizontalScrollViewD = new HorizontalScrollView(this);
+        horizontalScrollViewD.setId(R.id.horizontalScrollViewD);
+        scrollViewD.addView(horizontalScrollViewD);
+        tableRowMain2.addView(scrollViewD);
+        //done with all the 4 SECTIONS
+
+        tableLayoutMain.addView(tableRowMain1);
+        tableLayoutMain.addView(tableRowMain2);
+    }
+
+    private void createMatrix(){
+        int[][] tempMatrix = new int[mRow][mColumn];
+
+        for(int i=0; i<mRow; i++){
+            for(int j=0; j<mColumn; j++){
+                if(i==0 || j==0){   //for numbering complete 1st row and column
+                    if(i==0){
+                        tempMatrix[i][j] = j;  //traversing 1st row
+                    }
+                    if(j==0){
+                        tempMatrix[i][j] = i;  //traversing 1st column
+                    }
+                }
+                else{   //storing the multiplication in cells other than the 1st row and column
+                    tempMatrix[i][j] = i*j;
+                }
+            }
+        }
+        mTableMatrix = tempMatrix;
+    }
+
+    private void createTableB(){
+        TableLayout tableB = new TableLayout(this);
+        tableB.setId(R.id.tableB);
+        TableRow tableBRow = new TableRow(this);
+        tableBRow.setId(R.id.tableBRow);
+        //tableBRow.setMinimumWidth(40);
+
+        for(int i=1; i<=mColumn; i++){
+            TextView textView = new TextView(this);
+            textView.setText("" +i);
+            textView.setPadding(20,20,20,20);
+            //textView.setWidth(2);
+            tableBRow.addView(textView);
+        }
+        tableB.addView(tableBRow);
+
+        HorizontalScrollView hScroll = (HorizontalScrollView) findViewById(R.id.horizontalScrollViewB);
+        hScroll.addView(tableB);
+    }
+
+    private void createTableC(){
+        TableLayout tableC = new TableLayout(this);
+        tableC.setId(R.id.tableC);
+        //TableRow tableCrow = new TableRow(this);
+        //tableCrow.setId(R.id.tableCRow);
+
+        for(int i=1; i<=mRow; i++){
+            TableRow tableCrow = new TableRow(this);
+
+            TextView textView = new TextView(this);
+            textView.setText("" +i);
+            textView.setPadding(20,20,20,20);
+            tableCrow.addView(textView);
+            tableC.addView(tableCrow);
+        }
+
+        ScrollView scroll = (ScrollView) findViewById(R.id.scrollViewC);
+        scroll.addView(tableC);
+    }
+
+    private void createTableD(){
+        TableLayout tableD = new TableLayout(this);
+        tableD.setId(R.id.tableD);
+
+        for(int i=1; i<=mRow; i++){
+            TableRow tableRow = new TableRow(this);
+
+            for(int j=1; j<=mColumn; j++){
+                TextView textView = new TextView(this);
+                textView.setText("" +(i*j));
+                textView.setPadding(20,20,20,20);
+                tableRow.addView(textView);
+            }
+            tableD.addView(tableRow);
+        }
+
+        HorizontalScrollView hScroll = (HorizontalScrollView) findViewById(R.id.horizontalScrollViewD);
+        hScroll.addView(tableD);
+    }
+
+
+    private void createTableLayout(){
+
+        createMatrix();
+        createBasiclayout();
+        createTableB();
+        createTableC();
+        createTableD();
 
     }
 }
--- DynamicMuliplicationTable/app/src/main/java/com/example/android/dynamicmuliplicationtable/models/Table.java
+++ DynamicMuliplicationTable/app/src/main/java/com/example/android/dynamicmuliplicationtable/models/Table.java
@@ -2,20 +2,23 @@ package com.example.android.dynamicmuliplicationtable.models;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.android.dynamicmuliplicationtable.MatrixDisplayActivity;
+import com.example.android.dynamicmuliplicationtable.R;
 import com.example.android.dynamicmuliplicationtable.databinding.ActivityMatrixDisplayBinding;
 
 public class Table {
 
-    public int mRow=0;
-    public int mColumn=0;
-    public int mTableMatrix[][] ;
-    private Context mContext;
-    ActivityMatrixDisplayBinding mBinding;
+    public static int mRow=0;
+    public static int mColumn=0;
+
+    //Creating the tables and the scroll views for the FINAL Layout
+
 
     /*
     public int[][] sampleTable = {
@@ -24,13 +27,67 @@ public class Table {
     };
     */
 
-    public Table(int row, int column, Context context, ActivityMatrixDisplayBinding mBinding){
+    public Table(int row, int column){
         mRow = row;
         mColumn = column;
-        this.mContext = context;
-        this.mBinding = mBinding;
-        createMatrix();
-        createTableLayout();
+
+    }
+
+    public int getNumberOfRows(){
+        return mRow;
+    }
+
+    public int getNumberOfColumns(){
+        return mColumn;
+    }
+
+
+    /*
+    private void createBasiclayout(){
+        TableLayout tableLayoutMain = (TableLayout) mBinding.rootViewTableLayout;
+
+        //creating the A section table:
+        TableLayout tableLayoutA = new TableLayout(mContext);
+        tableLayoutA.setId(R.id.tableLayoutA);
+
+        TableRow tableRowA = new TableRow(mContext);
+        tableRowA.setId(R.id.tableRowA);
+
+        TextView textViewA = new TextView(mContext);
+        textViewA.setId(R.id.textViewA);
+        textViewA.setText("MultiTable");
+
+        tableRowA.addView(textViewA);
+        tableLayoutA.addView(tableRowA);
+        //A Section table created
+
+        //adding the A section table to the MAIN table:
+        TableRow tableRowMain = new TableRow(mContext);
+        tableRowMain.setId(R.id.tableRowMain);
+
+        tableRowMain.addView(tableLayoutA);
+        tableLayoutMain.addView(tableRowMain);
+        //added A section to the main table layout
+
+        //creating the COMPONENT B,C,D and adding it to the MainROW view
+        HorizontalScrollView horizontalScrollViewB = new HorizontalScrollView(mContext);
+        horizontalScrollViewB.setId(R.id.horizontalScrollViewB);
+        tableRowMain.addView(horizontalScrollViewB);
+        //created the B view - horizontalScrollViewB and added to the 1st COLUMN
+
+        //creating section C - scroll view and adding it to the main table layout:
+        TableRow tableRowC = new TableRow(mContext);
+        tableRowC.setId(R.id.tableRowC);
+        tableLayoutMain.addView(tableRowC);
+        ScrollView scrollView = new ScrollView(mContext);
+        tableRowC.addView(scrollView);
+        //done creating and adding it to the MAIN table layout
+
+        //creating the section D and adding it to the RowC layout:
+        HorizontalScrollView horizontalScrollViewD = new HorizontalScrollView(mContext);
+        horizontalScrollViewD.setId(R.id.horizontalScrollViewD);
+        tableRowC.addView(horizontalScrollViewD);
+        //done with all the 4 SECTIONS
     }
 
     private void createMatrix(){
@@ -54,32 +111,40 @@ public class Table {
         mTableMatrix = tempMatrix;
     }
 
+    private void createTableB(){
+        TableLayout tableB = new TableLayout(mContext);
+        tableB.setId(R.id.tableB);
+        TableRow tableBRow = new TableRow(mContext);
+        tableBRow.setId(R.id.tableBRow);
+
+        for(int i=1; i<=mColumn; i++){
+            TextView textView = new TextView(mContext);
+            textView.setText("" +i);
+            tableBRow.addView(textView);
+        }
+        tableB.addView(tableBRow);
+    }
+
+
     private void createTableLayout(){
         //TableLayout tableLayout = (TableLayout) mBinding.root_view_table_layout;
         TableLayout tableLayout = (TableLayout) mBinding.rootViewTableLayout;
         for(int i=0; i<mRow; i++){
             View view = new View(mContext);
-
             TableRow tableRow = new TableRow(mContext);
+
             for(int j=0; j<mColumn; j++){
                 TextView columnTextView = new TextView(mContext);
                 columnTextView.setText("" +mTableMatrix[i][j]);
                 //columnTextView.setWidth(35);
                 tableRow.addView(columnTextView);
             }
+
             tableLayout.addView(tableRow);
         }
     }
 
-    public int[][] getMatrix(){
-        return mTableMatrix;
-    }
+    */
 
-    public int getNumberOfRows(){
-        return mRow;
-    }
 
-    public int getNumberOfColumns(){
-        return mColumn;
-    }
 }
