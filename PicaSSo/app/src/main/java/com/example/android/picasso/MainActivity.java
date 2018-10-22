db665a3a7247853035f5970f9361313859fde607
rohegde7 <hegde.rohit7@gmail.com>
2018-10-22 10:15:10 +0530

    Implemented all features and tested successfully

--- PicaSSo/app/src/main/java/com/example/android/picasso/MainActivity.java
+++ PicaSSo/app/src/main/java/com/example/android/picasso/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView sampleImage = (ImageView) findViewById(R.id.image1);
 
-        Picasso.get().load("https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/02/google-pacman-796x419.jpg").into(sampleImage);
+        Picasso picassoImage = Picasso.get();
+
+        picassoImage.load("https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/02/google-pacman-796x419.jpg")
+                .networkPolicy(NetworkPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_STORE)
+                .memoryPolicy(MemoryPolicy.NO_CACHE).memoryPolicy(MemoryPolicy.NO_STORE)
+                .placeholder(R.drawable.ic_launcher_background).into(sampleImage);
+
+        picassoImage.setIndicatorsEnabled(true);
+
+        //Picasso.get().load(R.drawable.ic_computer_svgrepo_com).placeholder(R.drawable.ic_launcher_background).into(sampleImage);
     }
 }
